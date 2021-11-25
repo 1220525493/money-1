7922ef25a8ba608014efb727886039a124ebd9a1
alex <1220525493@qq.com>
2021-11-25 17:41:42 +0800

    创建了 layout 组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="nav-wrapper">
-  <div class="content">
-    Money.vue
+  <div>
+    <Layout>
+   <p>Money.vue</p>
+    </Layout>
   </div>
-  <Nav />
-  </div>
+
 </template>
 
 <script lang="ts">
@@ -14,14 +14,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper{
-  border: 1px solid green;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content{
-  overflow: auto;
-  flex-grow: 1;
-}
+
 </style>
