9f8e69cfe1554cfe7847f791314c3351e7521bac
alex <1220525493@qq.com>
2021-11-26 14:28:40 +0800

    创建 icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="nav">
     <router-link to="/money">
-      <svg>
-        <use xlink:href="#labels"></use>
-      </svg>
+      <Icon name="money"/>
       记账</router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <Icon name="labels"/>
+      标签</router-link>
     |
-    <router-link to="/statistics">统计</router-link></div>
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计</router-link></div>
 </template>
 
 <script lang="ts">
+
+
 import x from '@/assets/icons/labels.svg';
 
 console.log(x);
