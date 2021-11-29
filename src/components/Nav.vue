b0d3d40b4f22aec3513a0408d7a255a4dd32b92d
alex <1220525493@qq.com>
2021-11-29 15:51:35 +0800

    添加 全局样式和全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -24,6 +24,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "src/assets/style/helper.scss";
 nav{
 display: flex;
   box-shadow: 0 0 3px rgba(0,0,0,0.3);
@@ -42,7 +43,7 @@ display: flex;
     }
   }
   > .item.selected{
-    color: pink;
+    color: $color-highlight;
   }
 }
 
