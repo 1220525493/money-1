898fe57b1e3da8ff8ff37d3d600d9e4b272ee7c2
alex <1220525493@qq.com>
2021-12-07 15:55:59 +0800

    Money.vue 使用vuex

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -8,8 +8,7 @@
                   placeholder="在这里输入备注"
                   @update:value="onUpdateNotes"/>
       </div>
-
-     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
+     <Tags/>
     </Layout>
   </div>
 
@@ -21,26 +20,29 @@ import Types from "@/components/Money/Types.vue";
 import FromItem from "@/components/Money/FromItem.vue";
 import Tags from "@/components/Money/Tags.vue";
 import {Component, Vue,} from "vue-property-decorator";
-import store from "@/store/index2";
+
+
 
 
 
 @Component({
   components: {Tags,  FromItem, Types, NumberPad},
+  computed:{
+   recordList(){
+  return this.$store.state.recordList;
+   }
+  }
 })
 export default class Money extends Vue{
-  tags=store.tagList;
-  recordList=store.recordList;
   record:RecordItem={tags:[],notes:'',type:'-',amount:0};
-
-  onUpdateTags(value:string[]){
-    this.record.tags=value;
+  created(){
+    this.$store.commit('fetchRecords')
   }
   onUpdateNotes(value:string){
    this.record.notes=value;
   }
   saveRecord(){
- store.createRecord(this.record)
+ this.$store.commit('createRecord',this.record)
   }
 
 }
